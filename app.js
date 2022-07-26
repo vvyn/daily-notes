11b6a7f7c466655f080797c7842d54ea34efc1e0
vvyn <vicavyn3@gmail.com>
2022-07-25 20:57:35 -0500

    initialize project files and bootstrap design

---
+++ app.js
@@ -0,0 +1,32 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render("all");
+});
+
+app.get("/new", function(req, res) {
+  res.render("new");
+});
+
+app.get("/entry", function(req, res) {
+  res.render("entry");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
